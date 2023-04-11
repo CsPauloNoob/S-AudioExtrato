7cadc3af6583b3dc7d866f78890665e995f90464
CsPauloNoob <ps616131@gmail.com>
2023-04-10 23:04:39 -0500

    Adicionar arquivos de projeto.

---
+++ AudioExtrato/Controllers/ConverterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AudioExtrato.Controllers
+{
+    public class ConverterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AudioExtrato/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AudioExtrato.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
