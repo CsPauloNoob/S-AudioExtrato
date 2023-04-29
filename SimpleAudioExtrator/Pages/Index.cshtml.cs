59c50ff9316d011a27a55ea5397f0bd1ecc3a3bf
CsPauloNoob <ps616131@gmail.com>
2023-04-29 15:46:59 -0500

    Actions fix

--- SimpleAudioExtrator/Pages/Index.cshtml.cs
+++ SimpleAudioExtrator/Pages/Index.cshtml.cs
@@ -7,13 +7,31 @@ public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
 
+
+    [BindProperty]
+    public IFormFile File { get; set; }
+
+    public string Format { get; set; }
+
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
     }
 
-    public void OnGet()
+    public void OnPost()
     {
+        if (File != null)
+        {
+            // Obter o formato do arquivo de vídeo
+            Format = Path.GetExtension(File.FileName).ToLower();
+
+            // Verificar se o arquivo é de um formato de vídeo válido
+            if (Format != ".mp4" && Format != ".mkv" && Format != ".avi" && Format != ".wmv")
+            {
+                ModelState.AddModelError("File", "Selecione um arquivo de vídeo válido (.mp4, .mkv, .avi, .wmv)");
+                Format = null;
+            }
+        }
 
     }
 }
